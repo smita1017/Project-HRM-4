3c635a41bd2a737fc006e6f2afbf1e20717b9c36
smita1017 <smita126@outlook.com>
2025-02-21 11:06:55 +0530

    PROJECT-4-HRM

---
+++ index.js
@@ -0,0 +1,7 @@
+ let expandleft=document.querySelector(".expandleft")
+ let leftsection=document.querySelector(".leftsection")
+ expandleft.addEventListener("click",showleftsection)
+ function showleftsection(){
+    alert("hello")
+    leftsection.style.display="none"
+ }
