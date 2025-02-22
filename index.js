16d911178a297d34fd728e7146087a48eb82f425
smita1017 <smita126@outlook.com>
2025-02-22 19:02:19 +0530

    final

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 let expandleft = document.querySelector(".expandleft");
 let leftsection = document.querySelector(".leftsection");
 let rightsection = document.querySelector(".rightsection");
-
-expandleft.addEventListener("click", showleftsection);
-
-function showleftsection() {
-    let leftDisplay = window.getComputedStyle(leftsection).display;
-
-    if (leftDisplay === "none") {
-        leftsection.style.display = "block";
-        leftsection.style.width = "100%";
-        rightsection.style.display = "none";
+let closebtn=document.querySelector(".closebtn");
+expandleft.addEventListener("click", show);
+closebtn.addEventListener("click", hide);
 
 
-        // Ensure the expand button stays on top
-        expandleft.style.position = "relative";  // Ensure position is set
-        expandleft.style.zIndex = "1";
-    } else {
-        leftsection.style.display = "none";
-        rightsection.style.display = "block";
+function show(){
 
-        expandleft.style.position = "relative";  // Ensure position is set
-        expandleft.style.zIndex = "111111";
-    }
+leftsection.style.display="flex"
+leftsection.style.width="100%"
+leftsection.style.height="100%"
+closebtn.style.display="flex"
+}
+function hide(){
+leftsection.style.display="none"
+
 }
